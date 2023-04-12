9da50f194d881a5600fbd6b9cd65d0ce835f8c8c
FlaceyFurr <gontiefox@gmail.com>
2023-04-12 17:32:25 +0300

    Added validation inputs

--- script.js
+++ script.js
@@ -9,8 +9,47 @@ const daySpan = document.querySelector('#day-span');
 const monthSpan = document.querySelector('#month-span');
 const yearSpan = document.querySelector('#year-span');
 
-// Function to calculate and show age
+const dayError = document.querySelector('#day-error');
+const monthError = document.querySelector('#month-error');
+const yearError = document.querySelector('#year-error');
+
+// Validate inputs
+function validateInput() {
+    let isValid = true;
+    const day = parseInt(dayInput.value);
+    const month = parseInt(monthInput.value);
+    const year = parseInt(yearInput.value);
+
+    if (isNaN(day) || day < 1 || day > 31) {
+        dayError.style.display = "block"
+        isValid = false;
+    } else {
+        dayError.style.display = "none";
+    }
+
+    if (isNaN(month) || month < 1 || month > 12) {
+        monthError.style.display = "block"
+        isValid = false;
+    } else {
+        monthError.style.display = "none"
+    }
+
+    if (isNaN(year) || year < 1900 || year > new Date().getFullYear()) {
+        yearError.style.display = "block"
+        isValid = false;
+    } else {
+        yearError.style.display = "none"
+    }
+
+    return isValid;
+}
+
+// Calculate and show age
 function calculateAge() {
+    if (!validateInput()) {
+        return;
+    }
+
     const day = parseInt(dayInput.value);
     const month = parseInt(monthInput.value);
     const year = parseInt(yearInput.value);
